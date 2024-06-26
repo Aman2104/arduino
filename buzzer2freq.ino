fd18a864df0a59c77cbc0e818ab8cf8630b34c43
Aman2104 <amanayanikkodan@gmail.com>
2024-06-26 10:00:16 +0530

    buzzer2freq.ino

--- buzzer2freq.ino
+++ buzzer2freq.ino
@@ -8,7 +8,7 @@ void setup()
 void loop()
 {
   
-  tone(buzzer, 500); // send 1khz sound signal to buzzer
+  tone(buzzer, 500); // send 500hz sound signal to buzzer
   delay(1000); // Wait for 1000 millisecond(s)
   noTone(buzzer);
   delay(1000); // Wait for 1000 millisecond(s)
@@ -18,7 +18,7 @@ void loop()
   noTone(buzzer);
   delay(1000); // Wait for 1000 millisecond(s)
 
-  tone(buzzer, 1500); // send 1khz sound signal to buzzer
+  tone(buzzer, 1500); // send 1.5khz sound signal to buzzer
   delay(1000); // Wait for 1000 millisecond(s)
   noTone(buzzer);
   delay(1000); // Wait for 1000 millisecond(s)
